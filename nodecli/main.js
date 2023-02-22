4fbe85aea6d99847922fda95980a165fae59845c
mckyhrs <af.18.715@gmail.com>
2023-02-23 00:05:56 +0900

    first commit

---
+++ nodecli/main.js
@@ -0,0 +1,22 @@
+import { program } from "commander";
+import * as fs from "node:fs/promises";
+import { md2html } from "./md2html.js";
+
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
+const filePath = program.args[0];
+
+const options = program.opts();
+
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+}
+
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+    const html = md2html(file, cliOptions);
+    console.log(html);
+}).catch(err => {
+    console.log(err.message);
+    process.exit(1);
+});
